9f5fdf72e529ab536b6f673defa52f58f1e935b0
Rutuja <rutujabhandare61@gmail.com>
2017-07-29 20:10:10 +0530

    wish granting form

--- app/src/main/java/com/example/acer1/makeawish/MainActivity.java
+++ app/src/main/java/com/example/acer1/makeawish/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.acer1.makeawish;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showForm(View view) {
+        startActivity(new Intent(this, WishGrantingForm.class));
+    }
+
+    public void registerVolunteer(View view) {
+        startActivity(new Intent(this, VolunteerRegisterActivity.class));
     }
 }
