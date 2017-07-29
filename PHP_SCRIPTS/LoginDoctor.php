c1e18703ff2386c767861d9c7eae5504975a3f13
Bhavya Shah <bhavyashah239@gmail.com>
2017-07-30 03:32:08 +0530

    Donor login file

---
+++ PHP_SCRIPTS/LoginDoctor.php
@@ -0,0 +1,31 @@
+<?php
+
+         $email = $_GET['email'];
+         $password = $_GET['password'];
+
+         $con = mysqli_connect("localhost","root","root");
+         if (!$con)
+               {
+                         die('Could not connect: ' . mysql_error());
+                }
+
+           mysqli_select_db($con ,"root");
+
+           $query = "select Name from doctor where email='$email' AND password='$password'";
+            
+           $i=mysqli_query($con,$query);
+
+         $count = 0;
+
+          while($row = mysqli_fetch_array($i))
+            {
+             $count = $count+1;
+           //  $nm = $row['Name'];
+                             }
+          
+ if($count==1) {
+echo "1";
+}
+ mysqli_close($con);
+               
+    ?>
