4327508e892289753d268085987ddd030633ff3a
memonnabil821 <memonnabil821@gmail.com>
2017-07-29 20:17:54 +0000

    Fix

--- PHP_SCRIPTS/RegisterVolunteer.php
+++ PHP_SCRIPTS/RegisterVolunteer.php
@@ -9,7 +9,7 @@ $type = $_GET['type'];
 $phone = $_GET['phone'];
 
  $con = mysqli_connect('localhost','root','root','makeawish') or die('Unable to connect');
- $sql = "INSERT INTO volunteers (vid, email, password, name , gender, location, DOB, type) VALUES (NULL,'$email','$password','$name','$gender','$location','$dob','$type')";
+ $sql = "INSERT INTO volunteers (vid, email, password, name , gender, location, DOB, type,phone) VALUES (NULL,'$email','$password','$name','$gender','$location','$dob','$type','$phone')";
 
  if(mysqli_query($con,$sql)){
  echo '1';
