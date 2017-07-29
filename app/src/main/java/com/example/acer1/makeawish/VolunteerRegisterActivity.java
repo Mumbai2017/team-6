1486b8c5d04bd0d2e25e1322a31f9b4debdbcafe
Rutuja <rutujabhandare61@gmail.com>
2017-07-30 03:57:42 +0530

    doctor referal plus splash screen

--- app/src/main/java/com/example/acer1/makeawish/VolunteerRegisterActivity.java
+++ app/src/main/java/com/example/acer1/makeawish/VolunteerRegisterActivity.java
@@ -1,8 +1,12 @@
 package com.example.acer1.makeawish;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -10,10 +14,15 @@ import android.widget.Toast;
 
 public class VolunteerRegisterActivity extends AppCompatActivity {
 
+    Button btn;
+    int year_x,month_x,day_x;
+    static final int DIALOG_ID=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volunteer_register);
+        showDialogbtn();
+
     }
 
     public void registerVolunteer(View view) {
@@ -39,4 +48,37 @@ public class VolunteerRegisterActivity extends AppCompatActivity {
                 + type.getText().toString() + ","
                 + phone.getText().toString());
     }
+    ;
+    public void showDialogbtn(){
+        btn=(Button) findViewById(R.id.et_vol_dob);
+        btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showDialog(DIALOG_ID);
+                    }
+                }
+        );
+    }
+    private DatePickerDialog.OnDateSetListener datpickerListener
+            = new DatePickerDialog.OnDateSetListener(){
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            year_x=year;
+            month_x=month;
+            day_x=dayOfMonth;
+            Toast.makeText(VolunteerRegisterActivity.this,year_x+"/"+month_x+"/"+day_x, Toast.LENGTH_LONG).show();
+
+        }
+    };
+
+    protected Dialog onCreateDialog(int id){
+        if(id==DIALOG_ID){
+            return new DatePickerDialog(this, datpickerListener , year_x, month_x,day_x);
+        }
+        return null;
+
+    }
+
 }
