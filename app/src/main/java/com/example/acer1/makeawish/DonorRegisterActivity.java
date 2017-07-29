b803b3b9ac61b7c557ca5d60918cb27ea9d96765
psyclone20 <adnan.ans26@gmail.com>
2017-07-29 22:22:19 +0530

    Login and register activities setup

---
+++ app/src/main/java/com/example/acer1/makeawish/DonorRegisterActivity.java
@@ -0,0 +1,33 @@
+package com.example.acer1.makeawish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class DonorRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_register);
+    }
+
+    public void registerDonor(View view) {
+        EditText email = (EditText) findViewById(R.id.et_don_email);
+        EditText password = (EditText) findViewById(R.id.et_don_password);
+        EditText name = (EditText) findViewById(R.id.et_don_name);
+        EditText dob = (EditText) findViewById(R.id.et_vol_dob);
+        EditText phone = (EditText) findViewById(R.id.et_vol_phone);
+        EditText location = (EditText) findViewById(R.id.et_vol_location);
+
+//        new RegisterDonor(this.getApplicationContext()).execute(
+//                name.getText().toString() + ","
+//                        + email.getText().toString() + ","
+//                        + password.getText().toString() + ","
+//                        + name.getText().toString() + ","
+//                        + dob.getText().toString() + ","
+//                        + phone.getText().toString() + ","
+//                        + location.getText().toString());
+    }
+}
