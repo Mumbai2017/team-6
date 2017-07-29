fff52017bcb474563aa99792037a2f90d80f9f0a
Bhavya Shah <bhavyashah239@gmail.com>
2017-07-29 22:57:20 +0530

    Delete duplicate

--- Register.php
+++
@@ -1,9 +0,0 @@
-<?php
-$name = $_GET['name'];
-$addr = $_GET['addr'];
-$dob = $_GET['dob'];
-$phn = $_GET['phn'];
-$em = $_GET['em'];
-$user = $_GET['user'];
-$pass = $_GET['pass'];
-$bg = $_GET['blood'];
