59559d610adfb6d4d7a7a8964ad359ff97e56d02
bhavya216 <bhavyashah239@gmail.com>
2017-07-29 18:43:41 +0530

    Register Asynctask

---
+++ app/src/main/java/com/example/acer1/makeawish/RegisterVolunteer.java
@@ -0,0 +1,91 @@
+package com.example.acer1.makeawish;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+
+class RegisterVolunteer extends AsyncTask<String, Void, String> {
+    Context ctx;
+    
+
+    public RegisterVolunteer(Context context)
+    {
+        ctx = context;
+    }
+
+    protected String doInBackground(String... message) {
+        HttpClient httpclient;
+        HttpGet request;
+        HttpResponse response = null;
+        String result = "";
+        try {
+            String send = message[0];
+            String[] rec = send.split(",");
+            String name = rec[0];
+
+            String email = rec[1];
+
+            String password = rec[2];
+
+            String name = rec[3];
+
+            String gender = rec[4];
+
+            String location = rec[5];
+
+            String dob = rec[6];
+
+            String type = rec[7];
+			String free = rec[8];
+			String phone =rec[9];
+
+            httpclient = new DefaultHttpClient();
+            request = new HttpGet("RegisterVolunteer.php?email="+email+"&password="+password+"&name="+name+"&gender="+gender+"&location="+location+"&DOB="+dob+"&type="+type+"&free="+free+"&phone="+phone);
+            response = httpclient.execute(request);
+        } catch (Exception e) {
+            result = "error1";
+        }
+
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(
+                    response.getEntity().getContent()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result = result + line;
+            }
+        } catch (Exception e) {
+            Log.e("Error2","err "+e);
+            result = "error2";
+        }
+        return result;
+    }
+
+    protected void onPostExecute(String result) {
+
+        Log.e("Result", "Result" + result);
+        if (result.trim().equalsIgnoreCase("1"))
+        {
+            Toast.makeText(ctx,"Successfully Registered",Toast.LENGTH_LONG).show();
+            Intent i = new Intent(ctx, FirstActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            i.putExtra("Register","Yes");
+            i.putExtra("Username",usn);
+            ctx.startActivity(i);
+        }
+        else
+        {
+            Toast.makeText(ctx,"Unable to register please try again",Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+}
