357e9def353998f36f13b5ed65ff8dd13008eacf
bhavya216 <bhavyashah239@gmail.com>
2017-07-29 19:51:13 +0530

    PHP Script

---
+++ PHP_SCRIPTS/Register.php
@@ -0,0 +1,23 @@
+<?php
+$email = $_GET['email'];
+$password = $_GET['password'];
+$name = $_GET['name'];
+$gender = $_GET['gender'];
+$location = $_GET['location'];
+$dob = $_GET['DOB'];
+$type = $_GET['type'];
+
+$phone = $_GET['phone'];
+
+ $con = mysqli_connect('13.229.56.82','root','root') or die('Unable to connect');
+
+ $sql = "INSERT INTO volunteers (email,password,name,gender,location,DOB,type,phone) VALUES('$email','$password','$name',$gender,'$location','$dob','$type','$phone')";
+
+
+ if(mysqli_query($con,$sql)){
+ echo '1';
+ }
+else {
+echo 'Error';
+}
+  mysqli_close($con);
