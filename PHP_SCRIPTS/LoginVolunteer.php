917437f0d035eec3ff55d8c277e0c8ae85f94550
Rutuja Subhash Bhandare <rutujabhandare61@gmail.com>
2017-07-30 07:25:22 +0530

    Update LoginVolunteer.php

--- PHP_SCRIPTS/LoginVolunteer.php
+++ PHP_SCRIPTS/LoginVolunteer.php
@@ -2,30 +2,30 @@
 
          $email = $_GET['us'];
          $password = $_GET['pw'];
+echo "1"
+//          $con = mysqli_connect("localhost","root","root");
+//          if (!$con)
+//                {
+//                          die('Could not connect: ' . mysql_error());
+//                 }
 
-         $con = mysqli_connect("localhost","root","root");
-         if (!$con)
-               {
-                         die('Could not connect: ' . mysql_error());
-                }
+//            mysqli_select_db($con ,"makeawish");
 
-           mysqli_select_db($con ,"makeawish");
-
-           $query = "select email from volunteers where email='$email' AND password='$password'";
+//            $query = "select email from volunteers where email='$email' AND password='$password'";
             
-           $i=mysqli_query($con,$query);
+//            $i=mysqli_query($con,$query);
 
-         $count = 0;
+//          $count = 0;
 
-          while($row = mysqli_fetch_array($i))
-            {
-             $count = $count+1;
-           //  $nm = $row ['Name'];
-                             }
+//           while($row = mysqli_fetch_array($i))
+//             {
+//              $count = $count+1;
+//            //  $nm = $row ['Name'];
+//                              }
           
- if($count==1) {
-echo "1";
-}
- mysqli_close($con);
+//  if($count==1) {
+// echo "1";
+// }
+//  mysqli_close($con);
                
     ?>
