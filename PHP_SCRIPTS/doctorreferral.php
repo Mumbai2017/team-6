5d0761f1820bca2b0ad365ac5c0bc975b1d23009
Rutuja Subhash Bhandare <rutujabhandare61@gmail.com>
2017-07-30 10:33:13 +0530

    sadqa

--- PHP_SCRIPTS/doctorreferral.php
+++ PHP_SCRIPTS/doctorreferral.php
@@ -9,14 +9,17 @@ $dob = $_GET['DOB'];
 
 // $phone = $_GET['phone'];
  $con = mysqli_connect('localhost','root','root','makeawish') or die('Unable to connect');
- 
- $query ="select refname from volunteers where refname= '$refname' AND fathername = '$fathername' AND mothername = '$mothername'";
- $sql = "INSERT INTO doctorreferral VALUES ('$refname','$fathername','$mothername','$disease','$dob')";
 
- if(mysqli_query($con,$sql)){
- echo '1';
- }
-else if(mysqli_query($con,$query)){
-echo '2';
+ 
+ $query ="select count(refname) as cnt from volunteers where refname= '$refname' AND fathername = '$fathername' AND mothername = '$mothername'";
+$result=mysqli_query($con,$query);
+$row=mysqli_fetch_assco($result);
+if(row["cnt"]==0){
+$sql = "INSERT INTO doctorreferral VALUES ('$refname','$fathername','$mothername','$disease','$dob')";
+ mysqli_query($con,$sql);
+ echo "1";
+}
+else {
+echo "2";
 }
   mysqli_close($con);
