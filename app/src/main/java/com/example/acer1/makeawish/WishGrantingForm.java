c34bf0b3148b8d6e71ce456b00eb36cd3fbe46c9
Rutuja <rutujabhandare61@gmail.com>
2017-07-30 09:19:20 +0530

    111

--- app/src/main/java/com/example/acer1/makeawish/WishGrantingForm.java
+++ app/src/main/java/com/example/acer1/makeawish/WishGrantingForm.java
@@ -16,91 +16,18 @@ import android.widget.Toast;
 
 public class WishGrantingForm extends AppCompatActivity {
 
-    Button btn,btn1,btn2,btn3;
-    TextView dt1,dt2,dt3,dt;
+//    Button btn,btn1,btn2,btn3;
+//    TextView dt1,dt2,dt3,dt;
     int year_x,month_x,day_x;
     static final int DIALOG_ID=0;
-
+    String date="2017-07-30";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wish_granting_form);
-        showDialogbtn1();
-        showDialogbtn();showDialogbtn2();
-        showDialogbtn3();
-
-        dt = (TextView) findViewById(R.id.btn_pat_dob);
-        dt1 = (TextView) findViewById(R.id.btn_pat_dt_admn);
-        dt2 = (TextView) findViewById(R.id.btn_pat_dt_identified);
-        dt3 = (TextView) findViewById(R.id.btn_pat_proposed_dt);
-    }
-
-    public void showDialogbtn(){
-        btn=(Button) findViewById(R.id.et_pat_dob);
-        btn.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        showDialog(DIALOG_ID);
-                    }
-                }
-        );
-    }
-    private DatePickerDialog.OnDateSetListener datpickerListener
-            = new DatePickerDialog.OnDateSetListener(){
-
-        @Override
-        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-            year_x=year;
-            month_x=month;
-            day_x=dayOfMonth;
-            Toast.makeText(WishGrantingForm.this,year_x+"/"+month_x+"/"+day_x, Toast.LENGTH_LONG).show();
-
-        }
-    };
-
-    protected Dialog onCreateDialog(int id){
-        if(id==DIALOG_ID){
-            return new DatePickerDialog(this, datpickerListener , year_x, month_x,day_x);
-        }
-        return null;
 
     }
 
-    public void showDialogbtn1(){
-        btn1=(Button) findViewById(R.id.btn_pat_dt_admn);
-        btn1.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        showDialog(DIALOG_ID);
-                    }
-                }
-        );
-    }
-
-    public void showDialogbtn2(){
-        btn2=(Button) findViewById(R.id.btn_pat_dt_identified);
-        btn2.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        showDialog(DIALOG_ID);
-                    }
-                }
-        );
-    }
-    public void showDialogbtn3(){
-        btn3=(Button) findViewById(R.id.btn_pat_proposed_dt);
-        btn3.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        showDialog(DIALOG_ID);
-                    }
-                }
-        );
-    }
 
     public void submitGrantForm(View view) {
         EditText doc = (EditText) findViewById(R.id.et_pat_doc);
