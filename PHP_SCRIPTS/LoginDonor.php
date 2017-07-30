0fc76090ad7a7e662c7e7aac6b7033a2bc7688b4
Rutuja Subhash Bhandare <rutujabhandare61@gmail.com>
2017-07-30 07:51:35 +0530

    bhavya login donor

--- PHP_SCRIPTS/LoginDonor.php
+++ PHP_SCRIPTS/LoginDonor.php
@@ -1,33 +1,19 @@
 <?php
-
          $email = $_GET['us'];
          $password = $_GET['pw'];
-
-         $con = mysqli_connect("localhost","root","root","makeawish");
-         if (!$con)
-               {
-                         die('Could not connect: ' . mysql_error());
-                }
-
-     
-
-           $query = "select email from donor where email='$email' AND password='$password'";
+$con = mysqli_connect('localhost','root','root','makeawish') or die('Unable to connect');
+$query = "select email from donor where email='$email' AND password='$password'";
             
            $i=mysqli_query($con,$query);
-
          $count = 0;
-
           while($row = mysqli_fetch_array($i))
             {
              $count = $count+1;
-           //  $nm = $row['Name'];
+           //  $nm = $row ['Name'];
                              }
           
  if($count==1) {
 echo "1";
-}
-else{
-echo "hi".mysqli_error($con);
 }
  mysqli_close($con);
                
